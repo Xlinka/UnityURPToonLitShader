78b65ec19343a1782e09f51352736041507a1836
xLinka <Oliver.dunnington@outlook.com>
2023-09-04 12:48:39 +0100

    Refactoring
    Refactoring

--- NiloZOffset.hlsl
+++ NiloZOffset.hlsl
@@ -1,38 +1,29 @@
-// For more information, visit -> https://github.com/ColinLeung-NiloCat/UnityURPToonLitShaderExample
-
 #ifndef Include_NiloZOffset
 #define Include_NiloZOffset
 
-// Push an imaginary vertex towards camera in view space (linear, view space unit), 
-// then only overwrite original positionCS.z using imaginary vertex's result positionCS.z value
-// Will only affect ZTest ZWrite's depth value of vertex shader
-
+// Function to adjust the clip space position by pushing it toward or away from the camera in view space.
+// This function overwrites the original positionCS.z value to control depth during rendering.
 // Useful for:
-// -Hide ugly outline on face/eye
-// -Make eyebrow render on top of hair
-// -Solve ZFighting issue without moving geometry
-float4 NiloGetNewClipPosWithZOffset(float4 originalPositionCS, float viewSpaceZOffsetAmount)
+// - Hiding outline artifacts on the face or eyes
+// - Rendering eyebrows on top of hair
+// - Solving Z-fighting issues without altering geometry
+float4 ModifyClipPositionWithZOffset(float4 originalPositionCS, float viewSpaceZOffsetAmount)
 {
-    if(unity_OrthoParams.w == 0)
+    if (unity_OrthoParams.w == 0)
     {
-        ////////////////////////////////
-        //Perspective camera case
-        ////////////////////////////////
-        float2 ProjM_ZRow_ZW = UNITY_MATRIX_P[2].zw;
-        float modifiedPositionVS_Z = -originalPositionCS.w + -viewSpaceZOffsetAmount; // push imaginary vertex
-        float modifiedPositionCS_Z = modifiedPositionVS_Z * ProjM_ZRow_ZW[0] + ProjM_ZRow_ZW[1];
-        originalPositionCS.z = modifiedPositionCS_Z * originalPositionCS.w / (-modifiedPositionVS_Z); // overwrite positionCS.z
-        return originalPositionCS;    
+        // Perspective camera case
+        float2 projM_ZRow_ZW = UNITY_MATRIX_P[2].zw;
+        float modifiedPositionVS_Z = -originalPositionCS.w + -viewSpaceZOffsetAmount; // Push an imaginary vertex
+        float modifiedPositionCS_Z = modifiedPositionVS_Z * projM_ZRow_ZW[0] + projM_ZRow_ZW[1];
+        originalPositionCS.z = modifiedPositionCS_Z * originalPositionCS.w / (-modifiedPositionVS_Z); // Overwrite positionCS.z
+        return originalPositionCS;
     }
     else
     {
-        ////////////////////////////////
-        //Orthographic camera case
-        ////////////////////////////////
-        originalPositionCS.z += -viewSpaceZOffsetAmount / _ProjectionParams.z; // push imaginary vertex and overwrite positionCS.z
+        // Orthographic camera case
+        originalPositionCS.z += -viewSpaceZOffsetAmount / _ProjectionParams.z; // Push an imaginary vertex and overwrite positionCS.z
         return originalPositionCS;
     }
 }
 
 #endif
-
--- SimpleURPToonLitOutlineExample_LightingEquation.hlsl
+++ SimpleURPToonLitOutlineExample_LightingEquation.hlsl
@@ -1,74 +1,66 @@
-// For more information, visit -> https://github.com/ColinLeung-NiloCat/UnityURPToonLitShaderExample
-
-// This file is intented for you to edit and experiment with different lighting equation.
-// Add or edit whatever code you want here
-
-// #pragma once is a safe guard best practice in almost every .hlsl (need Unity2020 or up), 
-// doing this can make sure your .hlsl's user can include this .hlsl anywhere anytime without producing any multi include conflict
 #pragma once
 
-half3 ShadeGI(ToonSurfaceData surfaceData, ToonLightingData lightingData)
+// Function to calculate global illumination
+half3 CalculateGlobalIllumination(ToonSurfaceData surfaceData, ToonLightingData lightingData)
 {
-    // hide 3D feeling by ignoring all detail SH (leaving only the constant SH term)
-    // we just want some average envi indirect color only
+    // Sample spherical harmonics to approximate indirect lighting
     half3 averageSH = SampleSH(0);
 
-    // can prevent result becomes completely black if lightprobe was not baked 
-    averageSH = max(_IndirectLightMinColor,averageSH);
+    // Ensure a minimum indirect light color to prevent complete darkness
+    averageSH = max(_IndirectLightMinColor, averageSH);
 
-    // occlusion (maximum 50% darken for indirect to prevent result becomes completely black)
+    // Apply occlusion, limiting it to a maximum of 50% darkness
     half indirectOcclusion = lerp(1, surfaceData.occlusion, 0.5);
+
     return averageSH * indirectOcclusion;
 }
 
-// Most important part: lighting equation, edit it according to your needs, write whatever you want here, be creative!
-// This function will be used by all direct lights (directional/point/spot)
-half3 ShadeSingleLight(ToonSurfaceData surfaceData, ToonLightingData lightingData, Light light, bool isAdditionalLight)
+// Main lighting equation function (Edit this for custom lighting)
+half3 CalculateLighting(ToonSurfaceData surfaceData, ToonLightingData lightingData, Light light, bool isAdditionalLight)
 {
     half3 N = lightingData.normalWS;
     half3 L = light.direction;
-
-    half NoL = dot(N,L);
+    
+    half NoL = dot(N, L);
 
     half lightAttenuation = 1;
 
-    // light's distance & angle fade for point light & spot light (see GetAdditionalPerObjectLight(...) in Lighting.hlsl)
-    // Lighting.hlsl -> https://github.com/Unity-Technologies/Graphics/blob/master/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl
-    half distanceAttenuation = min(4,light.distanceAttenuation); //clamp to prevent light over bright if point/spot light too close to vertex
+    // Calculate distance attenuation for point and spot lights
+    half distanceAttenuation = min(4, light.distanceAttenuation);
 
-    // N dot L
-    // simplest 1 line cel shade, you can always replace this line by your own method!
-    half litOrShadowArea = smoothstep(_CelShadeMidPoint-_CelShadeSoftness,_CelShadeMidPoint+_CelShadeSoftness, NoL);
+    // Apply simple cel shading based on the dot product of normal and light direction
+    half litOrShadowArea = smoothstep(_CelShadeMidPoint - _CelShadeSoftness, _CelShadeMidPoint + _CelShadeSoftness, NoL);
 
-    // occlusion
+    // Apply occlusion
     litOrShadowArea *= surfaceData.occlusion;
 
-    // face ignore celshade since it is usually very ugly using NoL method
-    litOrShadowArea = _IsFace? lerp(0.5,1,litOrShadowArea) : litOrShadowArea;
+    // Handle facing direction to avoid cel shading artifacts
+    litOrShadowArea = _IsFace ? lerp(0.5, 1, litOrShadowArea) : litOrShadowArea;
 
-    // light's shadow map
-    litOrShadowArea *= lerp(1,light.shadowAttenuation,_ReceiveShadowMappingAmount);
+    // Apply shadow mapping from light source
+    litOrShadowArea *= lerp(1, light.shadowAttenuation, _ReceiveShadowMappingAmount);
 
-    half3 litOrShadowColor = lerp(_ShadowMapColor,1, litOrShadowArea);
+    half3 litOrShadowColor = lerp(_ShadowMapColor, 1, litOrShadowArea);
 
     half3 lightAttenuationRGB = litOrShadowColor * distanceAttenuation;
 
-    // saturate() light.color to prevent over bright
-    // additional light reduce intensity since it is additive
+    // Saturate light color to prevent over-brightening
+    // Reduce intensity for additional lights (additive lighting)
     return saturate(light.color) * lightAttenuationRGB * (isAdditionalLight ? 0.25 : 1);
 }
 
-half3 ShadeEmission(ToonSurfaceData surfaceData, ToonLightingData lightingData)
+// Function to handle emission
+half3 HandleEmission(ToonSurfaceData surfaceData, ToonLightingData lightingData)
 {
-    half3 emissionResult = lerp(surfaceData.emission, surfaceData.emission * surfaceData.albedo, _EmissionMulByBaseColor); // optional mul albedo
+    // Calculate emission and optionally multiply by albedo
+    half3 emissionResult = lerp(surfaceData.emission, surfaceData.emission * surfaceData.albedo, _EmissionMulByBaseColor);
     return emissionResult;
 }
 
-half3 CompositeAllLightResults(half3 indirectResult, half3 mainLightResult, half3 additionalLightSumResult, half3 emissionResult, ToonSurfaceData surfaceData, ToonLightingData lightingData)
+// Function to composite the final color
+half3 CompositeFinalColor(half3 indirectResult, half3 mainLightResult, half3 additionalLightSumResult, half3 emissionResult, ToonSurfaceData surfaceData, ToonLightingData lightingData)
 {
-    // [remember you can write anything here, this is just a simple tutorial method]
-    // here we prevent light over bright,
-    // while still want to preserve light color's hue
-    half3 rawLightSum = max(indirectResult, mainLightResult + additionalLightSumResult); // pick the highest between indirect and direct light
+    // Perform final color composition with brightness control
+    half3 rawLightSum = max(indirectResult, mainLightResult + additionalLightSumResult);
     return surfaceData.albedo * rawLightSum + emissionResult;
 }
